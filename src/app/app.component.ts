f611775496121f3199794a06d71fb468a663ddb4
Alexandre Bacellar <alexandre.bacellar@rededor.com.br>
2019-07-30 14:34:36 -0300

    Display by geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from './services/http.service';
+import { HttpService, Weather } from './services/http.service';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,35 @@ import { HttpService } from './services/http.service';
 })
 export class AppComponent implements OnInit {
   title = 'weathery';
+  weather: Weather;
+  weatherIcon: {
+    src: string;
+    alt: string;
+  };
 
   constructor(
     private httpService: HttpService
   ) { }
 
   ngOnInit() {
-    this.httpService.getWeatherByCityName('Rio').subscribe(console.log, console.error);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+        const coords = position.coords;
+        this.httpService.getWeatherByGeoCoord(coords.latitude, coords.longitude).subscribe(weather => {
+          console.log(weather);
+          this.weather = weather;
+          if (weather.weather.length) {
+            this.weatherIcon = {
+              src: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+              alt: weather.weather[0].description
+            };
+          }
+        }, () => alert('Error getting weather'));
+      });
+    } else {
+      alert('Geolocation not supported!');
+    }
   }
 
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+export interface Weather {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    message: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -23,24 +64,24 @@ export class HttpService {
   getWeatherByCityName(name: string) {
     let params = new HttpParams();
     params = params.set('q', name);
-    return this.get(this.getWeatherUrl, params);
+    return this.get<Weather>(this.getWeatherUrl, params);
   }
 
   getWeatherByCityID(id: number) {
     let params = new HttpParams();
     params = params.set('id', id.toString());
-    return this.get(this.getWeatherUrl, params);
+    return this.get<Weather>(this.getWeatherUrl, params);
   }
 
   getWeatherByGeoCoord(lat: number, lon: number) {
     let params = new HttpParams();
     params = params.set('lat', lat.toString());
     params = params.set('lon', lon.toString());
-    return this.get(this.getWeatherUrl, params);
+    return this.get<Weather>(this.getWeatherUrl, params);
   }
 
-  private get(url: string, params: HttpParams) {
-    return this.http.get(url, { params }).pipe(catchError(this.handleError));
+  private get<T>(url: string, params: HttpParams) {
+    return this.http.get<T>(url, { params }).pipe(catchError(this.handleError));
   }
 
   private handleError<T>(error: any, caught: Observable<T>) {
