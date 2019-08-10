4a8fcdc9dba97b08d36856964ad5dff224ef6eb3
Alexandre Bacellar <alexandre.bacellar@rededor.com.br>
2019-08-10 12:01:11 -0300

    Add citydb

---
+++ citydb.js
@@ -0,0 +1,73 @@
+const AdmZip = require('adm-zip');
+
+const zipName = 'city.list.json.zip';
+const dict = {};
+
+function parseName(name, separator = / |-/) {
+    return name.toLowerCase().split(separator).filter(Boolean).map(s => s.trim());
+}
+
+console.log(`Extracting ${zipName}...`);
+var zip = new AdmZip(zipName);
+var zipEntries = zip.getEntries();
+if (zipEntries.length < 1) {
+    throw new Error('Error: No city.list.json to unzip');
+}
+const cities = JSON.parse(zipEntries[0].getData().toString('utf8'));
+console.log(`Loaded ${cities.length} cities`);
+
+console.log('Indexing...');
+cities.forEach(city => {
+    const cityNames = parseName(city.name);
+    cityNames.forEach(name => {
+        if (!dict[name]) {
+            dict[name] = [];
+        }
+        dict[name].push(city);
+    });
+});
+console.log('Finished indexing');
+
+function find(query, top) {
+    const [cityName, country] = parseName(query, ',');
+    const entries = [];
+    const cityNames = parseName(cityName);
+    cityNames.forEach(name => {
+        const cities = dict[name];
+        if (!cities) return;
+        const weight = 1/cities.length;
+        cities.forEach(city => {
+            if (country && city.country.toLowerCase() !== country) return;
+            const i = entries.findIndex(entry => entry.city.id === city.id);
+            if (i === -1) {
+                entries.push({
+                    city, score: weight
+                });
+            } else {
+                entries[i].score += weight;
+            }
+        });
+    });
+    entries.forEach(entry => {
+        const scoreNames = parseName(entry.city.name);
+        scoreNames.forEach(name => {
+            const cities = dict[name];
+            if (!cities) return;
+            if (cityNames.includes(name)) return;
+            const weight = 1/cities.length;
+            entry.score -= weight;
+        });
+    });
+    return entries
+    .sort((entry1, entry2) => entry2.score - entry1.score)
+    .slice(0, top || entries.length);
+}
+module.exports.find = find;
+
+// const start = Date.now();
+// const found = find('Island, au', 50);
+// const dt = Date.now() - start;
+// found
+// .map(entry => `${entry.city.country}\t${entry.score.toFixed(20)}\t${entry.city.coord.lat},${entry.city.coord.lon}\t${entry.city.name}`)
+// .forEach(f => console.log(f));
+// console.log(`Done in ${dt} ms`);
